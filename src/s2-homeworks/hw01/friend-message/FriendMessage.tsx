da10747cf3544d7e7a16b98c2e9ab537aec50570
egorguzanov <hazinega@gmail.com>
2023-02-11 15:49:23 +0300

    ready HW2

--- src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -16,6 +16,7 @@ const FriendMessage = (props: MessagePropsType) => {
                     src={props.message.user.avatar}
                     //
                 />
+                <div className={s.angle}></div>
                 <div className={s.friendText}>
                     <div
                         id={'hw1-friend-name-' + props.message.id}
--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -15,27 +15,20 @@ const Message = (props: MessagePropsType) => {
             <div className={s.imageAndText}>
                 <img
                     id={'hw1-avatar-' + props.message.id} alt="avatar"
-                    // создаёт студент
                     src={props.message.user.avatar}
-                    //
                 />
+                <div className={s.angle}></div>
                 <div className={s.text}>
                     <div id={'hw1-name-' + props.message.id} className={s.name}>
-                        {/*создаёт студент*/}
                         {props.message.user.name}
-                        {/**/}
                     </div>
                     <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
-                        {/*создаёт студент*/}
                         {props.message.message.text}
-                        {/**/}
                     </pre>
                 </div>
             </div>
             <div id={'hw1-time-' + props.message.id} className={s.time}>
-                {/*создаёт студент*/}
                 {props.message.message.time}
-                {/**/}
             </div>
         </div>
     )
