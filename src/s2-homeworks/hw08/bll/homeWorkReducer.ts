f7ed7e7d1b9bdb3614f8a2f046a15d0e5db154aa
egorguzanov <hazinega@gmail.com>
2023-04-02 17:47:28 +0300

    hw8 done

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,39 @@
-import {UserType} from '../HW8'
+import {UserType} from "../HW8"
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+    | { type: "sort"; payload: "up" | "down" }
+    | { type: "check"; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case "sort": {
+            const copy = [...state]
+            if (action.payload === "up") {
+                copy.sort((a, b) => {
+                    if (a.name.toLowerCase() > b.name.toLowerCase()) {
+                        return 1;
+                    }
+                    if (a.name.toLowerCase() < b.name.toLowerCase()) {
+                        return -1;
+                    }
+                    return 0;
+                });
+            }
+            if (action.payload === "down") {
+                copy.sort((a, b) => {
+                    if (a.name.toLowerCase() < b.name.toLowerCase()) {
+                        return 1;
+                    }
+                    if (a.name.toLowerCase() > b.name.toLowerCase()) {
+                        return -1;
+                    }
+                    return 0;
+                });
+            }
+            return copy
         }
-        case 'check': {
-
-            return state // need to fix
+        case "check": {
+            return state.filter(el => el.age > action.payload)
         }
         default:
             return state
